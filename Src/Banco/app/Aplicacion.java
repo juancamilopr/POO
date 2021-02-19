4d0812d6f50e430a9d327111bbbe9e52305a6489
Juan Camilo <juan.patino4242@uco.net.co>
2021-02-19 00:11:28 -0500

    Banco

--- Src/Banco/app/Aplicacion.java
+++ Src/Banco/app/Aplicacion.java
@@ -1,8 +1,38 @@
 package Banco.app;
 
+import Banco.dominio.CuentaBancaria;
+import Banco.dominio.Persona;
+
 public class Aplicacion {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola Mundo!");
+
+        Persona JuanCamilo = new Persona();
+        Persona Matias = new Persona();
+
+        CuentaBancaria miCuenta = new CuentaBancaria();
+        CuentaBancaria cuentaMatias = new CuentaBancaria();
+
+        JuanCamilo.nombre = "Juan Camilo";
+        miCuenta.titular = JuanCamilo;
+        miCuenta.saldo = 800000;
+        miCuenta.estado = "A";
+
+        Matias.nombre = "Matias Cuartas";
+        cuentaMatias.titular = Matias;
+        cuentaMatias.saldo = 20000;
+        cuentaMatias.estado = "A";
+
+        miCuenta.retirar(200000);
+
+        System.out.println(miCuenta.consultarSaldo());
+        System.out.println(cuentaMatias.consultarSaldo());
+
+        boolean resultadoTransferencia = miCuenta.transferir(216000, cuentaMatias);
+
+        System.out.println(resultadoTransferencia);
+        System.out.println(miCuenta.consultarSaldo());
+        System.out.println(cuentaMatias.consultarSaldo());
     }
 }
 
---
+++ Src/Banco/dominio/Banco.java
@@ -0,0 +1,10 @@
+package Banco.dominio;
+
+
+import Banco.dominio.Persona;
+
+public class Banco {
+    public String nit;
+    public String nombre;
+    public Persona gerente;
+}
